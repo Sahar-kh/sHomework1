a4e0a44e1a5836f1ac7656f22ce96c8d3bd3ddf8
S-Kh22 <S_ashjaee_52@yahoo.com>
2020-05-11 17:10:13 +0200

    Add test for 'func1'

---
+++ test_ex11.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 11 16:48:16 2020
+
+@author: Sahar
+"""
+
+
+from ex1 import func1
+
+def test_ex11 ():
+    
+    assert func1 (1,2) == 27
+    
+    
