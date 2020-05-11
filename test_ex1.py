11806bea03878a0c465aea2def254a850bd98941
S-Kh22 <S_ashjaee_52@yahoo.com>
2020-05-11 17:09:40 +0200

    Add test for 'max_num'

---
+++ test_ex1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 11 16:39:38 2020
+
+@author: Sahar
+"""
+
+from ex1 import max_num
+
+def test_ex1 ():
+    #always diffirent number
+    assert max_num (5, 7, 9)==9
+    
+    
